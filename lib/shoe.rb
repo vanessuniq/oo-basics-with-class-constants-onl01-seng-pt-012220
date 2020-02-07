d51d3a2ced0972499a8f2781a608365d29e9bc0f
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-07 02:55:39 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS << @brand
+    BRANDS.uniq!
   end
+  
+  BRANDS.uniq
+  
 
   def cobble
     self.condition = "new"
